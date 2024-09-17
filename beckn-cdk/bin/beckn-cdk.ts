806509a0faf4bc757a67aa7a40f8a96324570b62
Monali Priyadarsini Behara <78998196+monali-2210@users.noreply.github.com>
2024-09-17 20:25:01 +0530

    adding helm stacks

--- beckn-cdk/bin/beckn-cdk.ts
+++ beckn-cdk/bin/beckn-cdk.ts
@@ -1,21 +1,34 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { StackProps } from 'aws-cdk-lib';
 import { VpcAlbStack } from '../lib/vpc-alb-stack';
 import { RdsStack } from '../lib/rds-stack';
 import { EksStack } from '../lib/eks-stack';
 import { RedisStack } from '../lib/redis-stack';
 import { DocumentDbStack } from '../lib/documentdb-stack';
 import { RabbitMqStack } from '../lib/rabbitmq-stack';
-import * as dotenv from 'dotenv';  // Import dotenv to load .env variables
+
+import { HelmRegistryStack } from '../lib/helm-registry';
+import { HelmGatewayStack } from '../lib/helm-gateway';
+import { HelmBAPStack } from '../lib/helm-beckn-bap';
+
+// import * as dotenv from 'dotenv';  // Import dotenv to load .env variables
 
 // Load environment variables from .env
-dotenv.config();
+// dotenv.config();
 
+import { ConfigProps, getConfig } from '../lib/config';
+
+const config = getConfig();
 const app = new cdk.App();
 
+type AwsEnvStackProps = StackProps & {
+  config: ConfigProps;
+};
+
 // Retrieve AWS Account ID and Region from the environment
-const accountId = process.env.AWS_ACCOUNT_ID;
-const region = process.env.AWS_REGION;
+const accountId = config.ACCOUNT;
+const region = config.REGION;
 
 if (!accountId || !region) {
   console.error("AWS_ACCOUNT_ID or AWS_REGION is missing from .env file");
@@ -28,24 +41,49 @@ const env = { account: accountId, region: region };
 // Function to deploy registry environment
 const deployRegistry = () => {
   const vpcAlbStack = new VpcAlbStack(app, 'RegistryVpcAlbStack', { env });
-  new EksStack(app, 'RegistryEksStack', { vpc: vpcAlbStack.vpc, env });
-  new RdsStack(app, 'RegistryRdsStack', { vpc: vpcAlbStack.vpc, env });
+  const eksStack = new EksStack(app, 'RegistryEksStack', { vpc: vpcAlbStack.vpc, env });
+  const rdsStack = new RdsStack(app, 'RegistryRdsStack', { config: config, vpc: vpcAlbStack.vpc, env });
+
+  new HelmRegistryStack(app, 'HelmRegistryStack', {
+    externalDomain: config.EXTERNAL_DOMAIN,
+    certArn: config.CERT_ARN,
+    chartName: config.CHART_NAME,
+    config: config,
+    eksCluster: eksStack.cluster,
+    rdsHost: rdsStack.rdsHost,
+  });
 };
 
 // Function to deploy gateway environment
 const deployGateway = () => {
   const vpcAlbStack = new VpcAlbStack(app, 'GatewayVpcAlbStack', { env });
-  new EksStack(app, 'GatewayEksStack', { vpc: vpcAlbStack.vpc, env });
-  new RdsStack(app, 'GatewayRdsStack', { vpc: vpcAlbStack.vpc, env });
+  const eksStack = new EksStack(app, 'GatewayEksStack', { vpc: vpcAlbStack.vpc, env });
+  const rdsStack = new RdsStack(app, 'GatewayRdsStack', { config: config, vpc: vpcAlbStack.vpc, env });
+
+  new HelmGatewayStack(app, 'HelmGatewayStack', {
+    config: config,
+    eksCluster: eksStack.cluster,
+    chartName: config.CHART_NAME,
+    externalDomain: config.EXTERNAL_DOMAIN,
+    certArn: config.CERT_ARN,
+    registryUrl: config.REGISTRY_URL,
+    rdsHost: rdsStack.rdsHost,
+  });
 };
 
 // Function to deploy BAP environment
 const deployBAP = () => {
   const vpcAlbStack = new VpcAlbStack(app, 'BapVpcAlbStack', { env });
-  new EksStack(app, 'BapEksStack', { vpc: vpcAlbStack.vpc, env });
+  const eksStack = new EksStack(app, 'BapEksStack', { vpc: vpcAlbStack.vpc, env });
   new DocumentDbStack(app, 'BapDocumentDbStack', { vpc: vpcAlbStack.vpc, env });
   new RedisStack(app, 'BapRedisStack', { vpc: vpcAlbStack.vpc, env });
   new RabbitMqStack(app, 'BapRabbitMqStack', { vpc: vpcAlbStack.vpc, env });
+
+  new HelmBAPStack(app, 'HelmBAPStack', {
+    config: config,
+    eksCluster: eksStack.cluster,
+  });
+
 };
 
 // Function to deploy BPP environment
@@ -61,7 +99,7 @@ const deployBPP = () => {
 const deploySandbox = () => {
   const vpcAlbStack = new VpcAlbStack(app, 'VpcAlbStack', { env });
   new EksStack(app, 'EksStack', { vpc: vpcAlbStack.vpc, env });
-  new RdsStack(app, 'RdsStack', { vpc: vpcAlbStack.vpc, env });
+  new RdsStack(app, 'RdsStack', { config: config, vpc: vpcAlbStack.vpc, env });
   new DocumentDbStack(app, 'DocumentDbStack', { vpc: vpcAlbStack.vpc, env });
   new RedisStack(app, 'RedisStack', { vpc: vpcAlbStack.vpc, env });
   new RabbitMqStack(app, 'RabbitMqStack', { vpc: vpcAlbStack.vpc, env });
